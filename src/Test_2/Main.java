064a38cd5dcadfd289e4fc5108085cae557dbd56
usoro999 <usoro999@gmail.com>
2022-07-05 20:47:29 +0300

    new project

---
+++ src/Test_2/Main.java
@@ -0,0 +1,13 @@
+package Test_2;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String a = "hello";
+        System.out.println(a.toUpperCase());
+        System.out.println(a);
+    }
+}
