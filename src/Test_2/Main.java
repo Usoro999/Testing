9ac3ccd1493496f18b7d769b4d1d08318ec10319
usoro999 <usoro999@gmail.com>
2022-07-06 21:32:37 +0300

    new progect

--- src/Test_2/Main.java
+++ src/Test_2/Main.java
@@ -1,13 +1,16 @@
 package Test_2;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
+   public static char getLastChar(String a){
+      return a.charAt(a.length() - 1);
+   }
+   public static void main(String[] args){
+       Scanner Stroka = new Scanner(System.in);
+       String text = Stroka.nextLine();
+       System.out.println(getLastChar(text));
 
-    public static void main(String[] args) {
-        String a = "hello";
-        System.out.println(a.toUpperCase());
-        System.out.println(a);
-    }
+   }
+
 }
