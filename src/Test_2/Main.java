5ce739265a4fd83a67aa51532d466249faabe625
usoro999 <usoro999@gmail.com>
2022-07-12 13:12:03 +0300

    new changes

--- src/Test_2/Main.java
+++ src/Test_2/Main.java
@@ -1,16 +1,25 @@
 package Test_2;
 
+
 import java.util.Scanner;
 
 public class Main {
-   public static char getLastChar(String a){
-      return a.charAt(a.length() - 1);
-   }
-   public static void main(String[] args){
-       Scanner geg = new Scanner(System.in);
-       String text = geg.nextLine();
-       System.out.println(getLastChar(text));
+    public static String getNumberExplanation(int num) {
+        String answer;
+        switch(num){
+            case 666:
+                return "devil number";
 
-   }
+            case 42:
+                return "answer for everything";
 
+            case 7:
+                return "prime number";
+
+        }
+        return "WF";
+    }
+    public static void main(String[] args){
+        getNumberExplanation(666);
+    }
 }
